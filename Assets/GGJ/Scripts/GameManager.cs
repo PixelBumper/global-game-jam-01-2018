3dd0e2b0fb883e40fed002ba32a09f68ef7e595a
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-27 00:54:59 +0100

    Implement timer in GameManager.

---
+++ Assets/GGJ/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	// Use this for initialization
+	void Start ()
+	{
+		var timer = TimeSpan.FromMinutes(3);
+		var interval = TimeSpan.FromSeconds(1);
+
+		Observable.Interval(interval)
+			.TakeUntil(Observable.Timer(timer))
+			.Select(_ => timer.Subtract(interval))
+			.Subscribe(
+				timeLeft => Debug.Log("Still alive for " + timeLeft),
+				Debug.LogException,
+				_ => Debug.Log("We're dead :(")
+			);
+	}
+}
