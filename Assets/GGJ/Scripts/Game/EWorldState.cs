84493429e104c3eb7fd642a8d578179066c5000a
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-27 14:04:43 +0100

    GameManager: Nuke key changes and implement world switching as well as highscore.

--- Assets/GGJ/Scripts/Game/EWorldState.cs
+++ Assets/GGJ/Scripts/Game/EWorldState.cs
@@ -1,5 +1,24 @@
-﻿public enum EWorldStatus
+﻿using System;
+
+public enum EWorldStatus
 {
     Ghost,
     Living,
+}
+
+static class EWorldStatusMethods
+{
+
+    public static EWorldStatus Advance(this EWorldStatus worldStatus)
+    {
+        switch (worldStatus)
+        {
+            case EWorldStatus.Living:
+                return EWorldStatus.Ghost;
+            case EWorldStatus.Ghost:
+                return EWorldStatus.Living;
+            default:
+                throw new NotSupportedException("Unknown world status" + worldStatus);
+        }
+    }
 }
