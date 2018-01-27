884b385b2bc800340fb34cd3eff86e1fff1ea2e5
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-27 17:56:08 +0100

    Add SpriteLayerOrderingHandler.

---
+++ Assets/GGJ/Scripts/Layering/SpriteLayerOrderingHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteLayerOrderingHandler : MonoBehaviour {
+	private SpriteRenderer _spriteRenderer;
+
+	void Start () {
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	private void FixedUpdate()
+	{
+		_spriteRenderer.sortingOrder = (int) (-transform.position.z * 10);
+	}
+}
