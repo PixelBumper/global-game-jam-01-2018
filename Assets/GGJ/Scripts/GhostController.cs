1928f791907b3c954d9ad595d13bb9a5c8960d16
pixelbumper <PixelBumper@users.noreply.github.com>
2018-01-27 19:00:15 +0100

    added ghost to main scene

--- Assets/GGJ/Scripts/GhostController.cs
+++ Assets/GGJ/Scripts/GhostController.cs
@@ -1,102 +1,129 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using GGJ.Scripts.ScriptableObjects;
+using UnityEditor;
 using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider), typeof(AudioSource))]
 public class GhostController : MonoBehaviour
 {
-	[SerializeField]
-	private float _range;
-	
-	[SerializeField]
-	private SingleNoteConfiguration[] _noteConfiguration;
+    [SerializeField]
+    private float _mumblingRange;
 
-	[SerializeField]
-	[TooltipAttribute("the time that has to pass between two sequence ")]
-	private float _delayBetweenSequenceRepetition;
+    [SerializeField]
+    private float _rangeForPlayingSequence;
 
-	private float _deltaSinceLastFinishedSequence;
+    [SerializeField]
+    private SingleNoteConfiguration[] _noteConfiguration;
 
-	private GameObject _currentPlayingNote;
-	private SpriteRenderer _spriteRendererOfCurrentNote;
+    [SerializeField]
+    [TooltipAttribute("the time that has to pass between two sequence ")]
+    private float _delayBetweenSequenceRepetition;
 
-	private AudioSource _audioSource;
-	private SphereCollider _sphereCollider;
-	
-	private int _nextNoteToPlay = 0;
-	private bool _isPlaying;
+    [SerializeField]
+    private AudioClip _mumblingSound;
 
-	// Use this for initialization
-	void Start ()
-	{
-		if (_noteConfiguration == null || _noteConfiguration.Length == 0)
-		{
-			// TODO init ghost with random note
-		}
-		_audioSource = GetComponent<AudioSource>();
+    private float _deltaSinceLastFinishedSequence;
 
-		GetComponent<SphereCollider>().radius = _range;
-		
-		_currentPlayingNote = new GameObject();
-		
-		_currentPlayingNote.SetActive(false);
-		var transform = _currentPlayingNote.transform;
-		transform.SetParent(this.transform);
-		
-		transform.position = new Vector2(this.transform.position.x, this.transform.position.y+1);
-		_spriteRendererOfCurrentNote = _currentPlayingNote.AddComponent<SpriteRenderer>();
-		_spriteRendererOfCurrentNote.sprite = _noteConfiguration[_nextNoteToPlay].Sprite;
-	}
+    private GameObject _currentPlayingNote;
+    private SpriteRenderer _spriteRendererOfCurrentNote;
 
-	private void FixedUpdate()
-	{
-		
-		if (_isPlaying && _audioSource.isPlaying == false)
-		{
-			_deltaSinceLastFinishedSequence += Time.deltaTime;
-			if (_deltaSinceLastFinishedSequence < _delayBetweenSequenceRepetition)
-			{
-				// hide note in case we are waiting for the delay to be over
-				_currentPlayingNote.SetActive(false);	
-			}
-			else
-			{
-				// play the next note in the sequence
-				_currentPlayingNote.SetActive(true);
-				_spriteRendererOfCurrentNote.sprite = _noteConfiguration[_nextNoteToPlay].Sprite;
-				_audioSource.PlayOneShot(_noteConfiguration[_nextNoteToPlay].Note);
-				_nextNoteToPlay++;
-				
-				// if this was the last note start waiting
-				if (_nextNoteToPlay >= _noteConfiguration.Length)
-				{
-					_nextNoteToPlay = 0;
-					_deltaSinceLastFinishedSequence = 0;
-				}
-			}
-		}
+    private AudioSource _audioSource;
+    private SphereCollider _sphereCollider;
 
-		
-	}
+    private int _nextNoteToPlay = 0;
+    private bool _isMumbling = true;
+    private bool _isPlayingSequence = false;
 
-	void OnTriggerEnter(Collider other)
-	{
-		if ("Player".Equals(other.gameObject.tag))
-		{
-			_currentPlayingNote.SetActive(true);
-			_isPlaying = true;
-			_nextNoteToPlay = 0;
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        if (_noteConfiguration == null || _noteConfiguration.Length == 0)
+        {
+            // TODO init ghost with random note
+        }
 
-	void OnTriggerExit(Collider other)
-	{
-		if ("Player".Equals(other.gameObject.tag))
-		{
-			_currentPlayingNote.SetActive(false);
-			_isPlaying = false;
-			_deltaSinceLastFinishedSequence = 0;
-		}
-	}
+        _audioSource = GetComponent<AudioSource>();
+
+        GetComponent<SphereCollider>().radius = _mumblingRange;
+
+        _currentPlayingNote = new GameObject();
+
+        _currentPlayingNote.SetActive(false);
+        var transform = _currentPlayingNote.transform;
+        transform.SetParent(this.transform);
+
+        transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + 1);
+        _spriteRendererOfCurrentNote = _currentPlayingNote.AddComponent<SpriteRenderer>();
+        _spriteRendererOfCurrentNote.sprite = _noteConfiguration[_nextNoteToPlay].Sprite;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isMumbling && _audioSource.isPlaying == false)
+        {
+            _audioSource.PlayOneShot(_mumblingSound);
+        }
+
+        if (_isPlayingSequence && _audioSource.isPlaying == false)
+        {
+            _deltaSinceLastFinishedSequence += Time.deltaTime;
+            if (_deltaSinceLastFinishedSequence < _delayBetweenSequenceRepetition)
+            {
+                // hide note in case we are waiting for the delay to be over
+                _currentPlayingNote.SetActive(false);
+            }
+            else
+            {
+                // play the next note in the sequence
+                _currentPlayingNote.SetActive(true);
+                _spriteRendererOfCurrentNote.sprite = _noteConfiguration[_nextNoteToPlay].Sprite;
+                _audioSource.PlayOneShot(_noteConfiguration[_nextNoteToPlay].Note);
+                _nextNoteToPlay++;
+
+                // if this was the last note start waiting
+                if (_nextNoteToPlay >= _noteConfiguration.Length)
+                {
+                    _nextNoteToPlay = 0;
+                    _deltaSinceLastFinishedSequence = 0;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if ("Player".Equals(other.gameObject.tag))
+        {
+            _isMumbling = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Vector3.Distance(other.transform.position, transform.position) < _rangeForPlayingSequence)
+        {
+            if (_isMumbling)
+            {
+                _audioSource.Stop();
+            }
+            _isMumbling = false;
+            _currentPlayingNote.SetActive(true);
+            _isPlayingSequence = true;
+        }
+        else
+        {
+            _isMumbling = true;
+            _currentPlayingNote.SetActive(false);
+            _isPlayingSequence = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if ("Player".Equals(other.gameObject.tag))
+        {
+            _isMumbling = false;
+        }
+    }
 }
