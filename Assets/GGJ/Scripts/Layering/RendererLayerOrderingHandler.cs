1c2c2f38d7c888137a2287dd72faefc77844e789
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-27 18:16:50 +0100

    Refactor SpriteLayerOrderingHandler into RendererLayerOrderingHandler and use it for the particle system too.

---
+++ Assets/GGJ/Scripts/Layering/RendererLayerOrderingHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RendererLayerOrderingHandler : MonoBehaviour {
+	private Renderer _renderer;
+
+	void Start ()
+	{
+		_renderer = GetComponent<Renderer>();
+	}
+
+	private void FixedUpdate()
+	{
+		_renderer.sortingOrder = (int) (-transform.position.z * 10);
+	}
+}
