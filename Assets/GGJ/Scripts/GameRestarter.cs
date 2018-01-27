723d99c057d86c91e688d25cb1709cb9814be0cd
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-27 22:58:48 +0100

    Implement Space to Restart Game.

---
+++ Assets/GGJ/Scripts/GameRestarter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameRestarter : MonoBehaviour {
+	private CompositeDisposable _compositeDisposable = new CompositeDisposable();
+
+	void Start ()
+	{
+		_compositeDisposable.Add(Observable.EveryUpdate()
+			.Where(_ => Input.GetKeyDown(KeyCode.Space))
+			.DelaySubscription(TimeSpan.FromSeconds(3))
+			.Subscribe(_ => SceneManager.LoadScene("Main")));
+	}
+
+	void OnDestroy ()
+	{
+		_compositeDisposable.Clear();
+	}
+}
