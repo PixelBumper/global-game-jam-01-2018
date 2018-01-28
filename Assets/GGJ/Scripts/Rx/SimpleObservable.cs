9898a238aa79bde8959705437876c87ce99bce7d
Niklas Baudy <niklas.baudy@vanniktech.de>
2018-01-28 03:31:49 +0100

    Sergio to the rescue.

--- Assets/GGJ/Scripts/Rx/SimpleObservable.cs
+++ Assets/GGJ/Scripts/Rx/SimpleObservable.cs
@@ -3,14 +3,36 @@
 class SimpleObservable<T> : IObservable<T>
 {
 	private Action<T> _action;
+	private Sergio<T> _last;
 
 	public void Subscribe(Action<T> action)
 	{
 		_action = action;
+
+		if (_last != null)
+		{
+			_action.Invoke(_last.t);
+			_last = null;
+		}
 	}
 
 	public void OnNext(T t)
 	{
-		_action.Invoke(t);
+		if (_action != null)
+		{
+			_action.Invoke(t);
+		}
+
+		_last = new Sergio<T>(t);
+	}
+
+	class Sergio<T>
+	{
+		public T t;
+
+		public Sergio(T t)
+		{
+			this.t = t;
+		}
 	}
 }
