90d8b419121a0ab8a8f354710fcb7a68852f944d
Sergio R. Lumley <lumley256@gmail.com>
2018-01-28 06:33:09 +0100

    Added initial instructions scene

---
+++ Assets/GGJ/Scripts/Utils/SceneSwitcherOnSpace.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcherOnSpace : MonoBehaviour
+{
+
+    [SerializeField] private string _nextSceneName;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(_nextSceneName);
+        }
+    }
+
+}
