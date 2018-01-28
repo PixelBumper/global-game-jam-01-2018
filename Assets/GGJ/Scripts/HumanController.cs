3c3cf344271e52c60f2ebf7ebff525b333b40f40
Sergio R. Lumley <lumley256@gmail.com>
2018-01-28 02:28:44 +0100

    Removed unnecessary colliders, using 3D spatial sound

--- Assets/GGJ/Scripts/HumanController.cs
+++ Assets/GGJ/Scripts/HumanController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UniRx;
 using UnityEngine;
 
-[RequireComponent(typeof(SphereCollider), typeof(AudioSource))]
+[RequireComponent(typeof(AudioSource))]
 public class HumanController : MonoBehaviour
 {
     [SerializeField] private float _mumblingRange;
@@ -16,7 +16,6 @@ public class HumanController : MonoBehaviour
     private List<SingleNoteConfiguration> _noteConfiguration;
     private GameObject _currentPlayingNote;
     private AudioSource _audioSource;
-    private SphereCollider _sphereCollider;
     private bool _canMumble = true;
     private IDisposable _dispisable;
 
@@ -24,7 +23,6 @@ public class HumanController : MonoBehaviour
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
-        GetComponent<SphereCollider>().radius = _mumblingRange;
     }
 
     public void SetNoteConfiguration(List<SingleNoteConfiguration> newConfig)
@@ -32,33 +30,11 @@ public class HumanController : MonoBehaviour
         _noteConfiguration = newConfig;
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        if ("Player".Equals(other.gameObject.tag))
+        if (!_audioSource.isPlaying)
         {
-            if(_canMumble)
-            {
-                _canMumble = false;
-
-                _dispisable = Observable.Interval(TimeSpan.FromSeconds(_mumblingSound.length))
-                    .Subscribe(_ => { }, Debug.LogException, OnMumbleFinished);
-
-                _audioSource.PlayOneShot(_mumblingSound);
-            }
-        }
-    }
-
-    private void OnMumbleFinished()
-    {
-        Debug.Log("OnMumbleFinished");
-        _canMumble = true;
-    }
-
-    private void OnDestroy()
-    {
-        if(_dispisable != null)
-        {
-            _dispisable.Dispose();
+            _audioSource.PlayOneShot(_mumblingSound);
         }
     }
 
@@ -66,7 +42,7 @@ public class HumanController : MonoBehaviour
     {
         _mumblingSound = mumble;
         GameObject prefab;
-        if(_charactersMap.MumbleToHumans.TryGetValue(mumble, out prefab))
+        if (_charactersMap.MumbleToHumans.TryGetValue(mumble, out prefab))
         {
             Instantiate(prefab, transform);
         }
