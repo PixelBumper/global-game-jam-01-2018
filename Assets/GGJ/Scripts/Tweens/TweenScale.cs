f24fef848ff649928c7db3595b029e44ea784517
exAntares <luis.andres81@gmail.com>
2018-01-28 13:45:07 +0100

    Quick TweenScale component for the ScoreScene

---
+++ Assets/GGJ/Scripts/Tweens/TweenScale.cs
@@ -0,0 +1,59 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TweenScale : MonoBehaviour
+{
+    public enum LoopType
+    {
+        NoLoop,
+        Repeat,
+        Yoyo,
+    }
+
+    public enum LoopState
+    {
+        Forward,
+        Backward,
+    }
+
+    public bool AutoStart = true;
+    public LoopType Loop = LoopType.NoLoop;
+    public float Duration = 1;
+    public Vector3 EndValue = Vector3.one;
+    private Vector3 _initialValue;
+    private Vector3 _targetValue;
+    private LoopState _state = LoopState.Forward;
+
+    private void Start ()
+    {
+        _initialValue = transform.localScale;
+        _targetValue = EndValue;
+        if (AutoStart)
+        {
+            StartTween();
+        }
+	}
+    
+    public void StartTween()
+    {
+        var tweener = transform.DOScale(_targetValue, Duration);
+        tweener.Play();
+        tweener.onComplete = TweenCompleted;
+    }
+
+    private void TweenCompleted()
+    {
+        switch(Loop)
+        {
+            case LoopType.Repeat:
+                StartTween();
+                break;
+            case LoopType.Yoyo:
+                _state = _state == LoopState.Forward ? LoopState.Backward : LoopState.Forward;
+                _targetValue = _state == LoopState.Forward ? EndValue : _initialValue;
+                StartTween();
+                break;
+        }
+    }
+}
