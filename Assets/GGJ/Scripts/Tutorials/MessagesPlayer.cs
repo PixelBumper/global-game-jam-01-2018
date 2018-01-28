c2e5175aefccf18015d5a4819884e95b1179b213
Sergio R. Lumley <lumley256@gmail.com>
2018-01-28 07:36:08 +0100

    Added story and sound instructions to game

---
+++ Assets/GGJ/Scripts/Tutorials/MessagesPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MessagesPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _audioClips;
+
+    [SerializeField] private AudioSource _audioSource;
+
+    private int _nextIndexToPlay;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!_audioSource.isPlaying)
+        {
+            _audioSource.PlayOneShot(_audioClips[_nextIndexToPlay]);
+            _nextIndexToPlay = (_nextIndexToPlay + 1) % _audioClips.Length;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _audioSource.Stop();
+    }
+}
